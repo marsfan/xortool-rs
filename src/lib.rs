21264e6a85cb71b7712c31d6831a4106cdb1e850
Gabe R. <9311953+marsfan@users.noreply.github.com>
2025-11-23 16:07:34 -0700

    Read version from cargo metadata
    Signed-off-by: Gabe R. <9311953+marsfan@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,5 @@ mod routine;
 pub mod tool_main;
 pub mod tool_xor;
 
-// FIXME: Read this from crate info?
-const VERSION: &str = "1.0.2";
+// https://stackoverflow.com/a/27841363
+const VERSION: &str = env!("CARGO_PKG_VERSION");
